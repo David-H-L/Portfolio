2916c66857fb05be4285863811b75d87083ec397
David Huanca Ledezma <davidhuancaledezma@gmail.com>
2025-10-04 23:54:49 -0400

    feat: add initial structure

---
+++ src/components/data/Projects.ts
@@ -0,0 +1,39 @@
+export const projects = [
+    {
+        name:"Git-github(course)",
+        linkGithub:"https://github.com/David-H-L/Curso-Git-Github",
+        linkDeploy:"",
+        img:"/public/img/git_github.jpg",
+        deploy: false,
+        date:"April 2025",
+        type:"project"
+    },
+    {
+        name:"Restaurant(web page)",
+        linkGithub:"https://github.com/David-H-L/Proyecto-final-SCESI",
+        linkDeploy:"https://proyecto-final-scesi.pages.dev/",
+        img:"/public/img/restaurant.png",
+        deploy: true,
+        date: "June 2025",
+        type:"project"
+    },
+    {
+        name:"Cinema (api rest)",
+        linkGithub:"https://github.com/David-H-L/API-REST",
+        linkDeploy:"",
+        img:"/public/img/cinema.webp",
+        deploy: false,
+        date:"August 2025",
+        type:"project"
+
+    },
+    {
+        name:"Subastas (movile aplication)",
+        linkGithub:"https://github.com/JJ-HEsquivel/subastas_app",
+        linkDeploy:"",
+        img:"/public/img/subasta.webp",
+        deploy: false,
+        date:"March 2025",
+        type:"project"
+    }
+]
