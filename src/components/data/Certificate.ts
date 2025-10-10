48b2046df10830fcbaf6b628e109010a139f89f6
David Huanca Ledezma <davidhuancaledezma@gmail.com>
2025-10-10 01:00:51 -0400

    feat: add data certificate

---
+++ src/components/data/Certificate.ts
@@ -0,0 +1,38 @@
+export const certificate = [
+    {
+        name:"Redes de computadoras",
+        linkDeploy:"",
+        img:"/img/test_certificate.webp",
+        deploy:false,
+        date:"June 2025",
+        type:"certificate"
+
+    },
+    {
+        name:"Redes de computadoras",
+        linkDeploy:"",
+        img:"/img/test_certificate.webp",
+        deploy:false,
+        date:"June 2025",
+        type:"certificate"
+
+    },
+    {
+        name:"Redes de computadoras",
+        linkDeploy:"",
+        img:"/img/test_certificate.webp",
+        deploy:false,
+        date:"June 2025",
+        type:"certificate"
+
+    },
+    {
+        name:"Redes de computadoras",
+        linkDeploy:"",
+        img:"/img/test_certificate.webp",
+        deploy:false,
+        date:"June 2025",
+        type:"certificate"
+
+    }
+]
